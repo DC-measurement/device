3c4b1a2918a7e245c9e29503cfd47f5abd42d187
senbeigarigari <2216042@ed.tus.ac.jp>
2019-09-25 16:41:34 +0900

    a

---
+++ Q5.py
@@ -0,0 +1,40 @@
+f = open('writeme.xlsx', 'w')
+
+f.write('1st ')
+f.write("line\n")
+f.writelines(['2nd line\n', 'last line'])
+
+f.close()
+
+f = open('writeme.xlsx', 'r')
+for s in f:
+    print(s)
+f.close()
+
+f = open('writeme.xlsx', 'w')
+
+f.write('2: 1st line\n')
+f.write('2: 2nd line\n')
+f.write('2: last line\n')
+
+f.close()
+
+print('-----------')
+f = open('writeme.xlsx', 'r')
+for s in f:
+    print(s)
+f.close()
+
+f = open('writeme.xlsx', 'a')
+
+f.write('3: 1st line\n')
+f.write('3: 2nd line\n')
+f.write('3: last line\n')
+
+f.close()
+
+print('-----------')
+f = open('writeme.xlsx', 'r')
+for s in f:
+    print(s)
+f.close()
