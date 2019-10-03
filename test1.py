309f7aa0582f2b2822c29eb43a5e6a888ad79a6d
SHU023 <shu.rarara21@gmail.com>
2019-10-03 22:44:59 +0900

    make a graph

--- test1.py
+++ test1.py
@@ -5,13 +5,18 @@ import random
 import datetime
 import matplotlib.pyplot as plt
 
+rm =visa.ResourceManager()
+rm.list_resources()
+instr=rm.open_resource('USB0::0x2A8D::0x0201::MY57700883::0::INSTR')
+
+
 ms_A=[]
 ms_V=[]
 ms_R=[]
 for i in range(10):
-    I=random.random()
-    V=random.random()
-    R=random.random()
+    I=float(instr.query("MEAS:CURR:DC? 100 mA"))
+    V=float(instr.query("MEAS:VOLT:DC?"))
+    R=float(instr.query("MEAS:RES?"))
     ms_A.append(I)
     ms_V.append(V)
     ms_R.append(R)
